fa1ef2f2dbb23aa9c3a5c1470c3bdfbade4c278d
supremgy <87310605+supremgy@users.noreply.github.com>
2024-03-02 22:41:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,33 +1,19 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-int a[9],sum;
-pair<int,int> ret;
-vector<int>v;
-void solve(){
-    for(int i =0;i<9;i++){
-        for(int j =0;j<i;j++){
-        if(sum - a[i] - a[j]==100){
-            ret ={i,j};
-            return;
-        }
-    }
-    }
-}
-
+int a[9];
 int main (){
-    for(int i =0;i<9;i++){
-        cin >> a[i]; sum+=a[i];
+    for(int i=0;i<9;i++){
+        cin >> a[i];
     }
-    solve();
-    for(int i =0;i<9;i++){
-        if(ret.first == i || ret.second == i) continue;
-        v.push_back(a[i]);
-    }
-    sort(v.begin(),v.end());
-    for(int i : v) cout << i << " ";
+    sort(a,a+9);
+    do{
+        int sum = 0;
+        for(int i =0;i<7;i++) sum+= a[i];
+            if(sum == 100)break;
+        
+        
+    }while(next_permutation(a, a+9));
+    for(int i =0;i<7;i++) cout << a[i] << "\n";
+    
     return 0;
-    
-    
 }
