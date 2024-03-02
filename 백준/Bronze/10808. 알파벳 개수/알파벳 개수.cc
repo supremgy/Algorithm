40b4d2cea7fafe688dd1413774773366cd4ed8f4
supremgy <87310605+supremgy@users.noreply.github.com>
2024-03-02 23:46:10 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+string str;
+int cnt[26];
+int main(){
+    cin >> str;
+    for(char a: str){
+        cnt[a-'a']++;
+    }
+    for(int i=0;i<26;i++) cout<< cnt[i] << ' ';
+    return 0;
+}
