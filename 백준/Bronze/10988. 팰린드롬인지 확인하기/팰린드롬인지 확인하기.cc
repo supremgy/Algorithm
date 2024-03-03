9e57fa00005ab6e6535f4dfa009997bee2c9b0c0
supremgy <87310605+supremgy@users.noreply.github.com>
2024-03-03 13:57:33 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-string str;
+string str,re;
 int main(){
 cin >> str;
-  string reversedStr(str.rbegin(),str.rend());
-  if(str == reversedStr){
-    cout << 1 << "\n";
-  }else{
-          cout << 0 << "\n";
-  }
-
+re  = str;
+reverse(re.begin(),re.end());
+if(str == re) cout << 1 << "\n";
+else cout << 0<<"\n";
     return 0;
 }
